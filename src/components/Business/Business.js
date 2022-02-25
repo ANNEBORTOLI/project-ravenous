4a0daa51b6e74f8bc9b53e326dc086ef57eacccc
ANNEBORTOLI <annebortoli@gmail.com>
2022-02-25 16:41:34 -0300

    :art:Interact with the Yelp API

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -11,7 +11,7 @@ class Business extends React.Component {
             alt={this.props.business.name}
           />
         </div>
-        <h2>MarginOtto Pizzeria</h2>
+        <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
             <p>{this.props.business.address}</p>
